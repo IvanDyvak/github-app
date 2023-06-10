020ae54b858e6a4a988d52c72ddfb285e2e7e029
Ivan <you@example.com>
2023-06-10 23:37:11 +0300

    update

--- src/components/RepoMenu.js
+++ src/components/RepoMenu.js
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import { useIterator } from "../customHooks/";
 
 export default function RepoMenu({ repositories, onSelect = f => f }) {
-    const [{ name }, previous, next] = useIterator(repositories);
+    const [{ name, html_url, description }, previous, next] = useIterator(repositories);
+    console.log(repositories);
 
     useEffect(() => {
         if (!name) return;
@@ -10,10 +11,13 @@ export default function RepoMenu({ repositories, onSelect = f => f }) {
     }, [name]);
 
     return (
-        <div style={{ display: "flex", width:'200',alignItems:"center",justifyContent: "space-around" }}>
-            <button onClick={previous}>&lt;</button>
-            <p>{name}</p>
-            <button onClick={next}>&gt;</button>
-        </div>
-    );
+        <>
+            <div style={{ display: "flex", width:'200',alignItems:"center",justifyContent: "space-around" }}>
+                <button onClick={previous}>&lt;</button>
+                <p><a target="_blank" href={html_url}>{name}</a></p>
+                <button onClick={next}>&gt;</button>
+            </div>
+            <p>{description}</p>
+        </>
+);
 }
